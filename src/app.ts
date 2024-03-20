08c0e37d92f1674e4f0286331823c78715f00c6b
Juliano Alves <contato@julianoalves.me>
2024-03-20 12:03:42 -0300

    feat: 🎸 remove dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
-import { PrismaClient } from '@prisma/client';
-import fastify from 'fastify';
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
 
 export const app = fastify({ logger: true });
 
 const prisma = new PrismaClient();
+
+app.post("/user", async (request, reply) => {
+  const { name, email } = request.body;
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  reply.send(user);
+});

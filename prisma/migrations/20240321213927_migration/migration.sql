25082409283fe2ae737530282c2b7b90a98fa886
Juliano Alves <julianoalves@me>
2024-03-21 19:14:18 -0300

    feat: 🎸 add new schema migration

--- prisma/migrations/20240321213927_migration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `password_hash` on the `users` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(100)`.
-
-*/
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "password_hash" SET DATA TYPE VARCHAR(100);
